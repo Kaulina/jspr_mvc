019da99eb3a720cbd94dd776af8c9536ea625f76
Kaulina <iv_kaulina@mail.ru>
2024-07-29 16:24:05 +0700

    inition commit

---
+++ src/test/java/ru/netology/servlet/WebTestConfig.java
@@ -0,0 +1,12 @@
+package ru.netology.servlet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.netology")
+public class WebTestConfig {
+
+}
